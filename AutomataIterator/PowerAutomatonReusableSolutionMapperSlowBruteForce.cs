1576ade7a9b902db3fee56fdd712a0d055fa3bc1
Kazimierz Wojciechowski <kazimierz.9200@gmail.com>
2018-11-11 01:20:52 +0100

    Preparing for brute force integrity tests - how it should have been in the first place

---
+++ AutomataIterator/PowerAutomatonReusableSolutionMapperSlowBruteForce.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreDefinitions;
+
+namespace AutomataIterator
+{
+    public class PowerAutomatonReusableSolutionMapperSlowBruteForce : ISolutionMapperReusable
+    {
+        public IEnumerable<ISolvedOptionalAutomaton> SelectAsSolved(IEnumerable<IOptionalAutomaton> problemsToSolve) => throw new NotImplementedException();
+    }
+}
