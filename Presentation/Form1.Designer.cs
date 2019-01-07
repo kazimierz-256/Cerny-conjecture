cdf900d8b41bacd7fab2ce4d6ef24d2383999659
michalina <tumialism@student.mini.pww.edu.pl>
2019-01-07 23:22:19 +0100

    dodanie wykresu kolowego do prezentacji

--- Presentation/Form1.Designer.cs
+++ Presentation/Form1.Designer.cs
@@ -28,6 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.logBox = new System.Windows.Forms.RichTextBox();
             this.prompt = new MaterialSkin.Controls.MaterialRaisedButton();
             this.close = new MaterialSkin.Controls.MaterialRaisedButton();
@@ -39,14 +43,17 @@
             this.tabStatistics = new System.Windows.Forms.TabPage();
             this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
             this.materialLabelAdres = new MaterialSkin.Controls.MaterialLabel();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.materialTabControl1.SuspendLayout();
             this.tabSettings.SuspendLayout();
             this.tabStatistics.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // logBox
             // 
-            this.logBox.Location = new System.Drawing.Point(17, 282);
+            this.logBox.Location = new System.Drawing.Point(782, 417);
             this.logBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.logBox.Name = "logBox";
             this.logBox.Size = new System.Drawing.Size(303, 90);
@@ -133,6 +140,9 @@
             // 
             // materialTabControl1
             // 
+            this.materialTabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.materialTabControl1.Controls.Add(this.tabSettings);
             this.materialTabControl1.Controls.Add(this.tabStatistics);
             this.materialTabControl1.Depth = 0;
@@ -140,7 +150,7 @@
             this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialTabControl1.Name = "materialTabControl1";
             this.materialTabControl1.SelectedIndex = 0;
-            this.materialTabControl1.Size = new System.Drawing.Size(1052, 578);
+            this.materialTabControl1.Size = new System.Drawing.Size(1239, 578);
             this.materialTabControl1.TabIndex = 10;
             // 
             // tabSettings
@@ -159,25 +169,29 @@
             // 
             // tabStatistics
             // 
+            this.tabStatistics.Controls.Add(this.chart1);
+            this.tabStatistics.Controls.Add(this.materialDivider1);
             this.tabStatistics.Controls.Add(this.materialLabel1);
             this.tabStatistics.Controls.Add(this.logBox);
             this.tabStatistics.Controls.Add(this.materialLabel3);
             this.tabStatistics.Location = new System.Drawing.Point(4, 25);
             this.tabStatistics.Name = "tabStatistics";
             this.tabStatistics.Padding = new System.Windows.Forms.Padding(3);
-            this.tabStatistics.Size = new System.Drawing.Size(1044, 537);
+            this.tabStatistics.Size = new System.Drawing.Size(1231, 549);
             this.tabStatistics.TabIndex = 1;
             this.tabStatistics.Text = "STATYSTYKI";
             this.tabStatistics.UseVisualStyleBackColor = true;
             // 
             // materialTabSelector1
             // 
+            this.materialTabSelector1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.materialTabSelector1.BaseTabControl = this.materialTabControl1;
             this.materialTabSelector1.Depth = 0;
             this.materialTabSelector1.Location = new System.Drawing.Point(-6, 63);
             this.materialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialTabSelector1.Name = "materialTabSelector1";
-            this.materialTabSelector1.Size = new System.Drawing.Size(1060, 56);
+            this.materialTabSelector1.Size = new System.Drawing.Size(1247, 56);
             this.materialTabSelector1.TabIndex = 11;
             this.materialTabSelector1.Text = "materialTabSelector1";
             // 
@@ -194,11 +208,44 @@
             this.materialLabelAdres.TabIndex = 7;
             this.materialLabelAdres.Text = "Adres:";
             // 
+            // materialDivider1
+            // 
+            this.materialDivider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Location = new System.Drawing.Point(6, 238);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(341, 305);
+            this.materialDivider1.TabIndex = 10;
+            this.materialDivider1.Text = "materialDivider1";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(28, 253);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "UnaryFinishedSeries";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(300, 275);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            title1.Name = "Przeanalizowane automaty unarne";
+            title1.Text = "Przeanalizowane automaty unarne";
+            this.chart1.Titles.Add(title1);
+            // 
             // Presentation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1054, 702);
+            this.ClientSize = new System.Drawing.Size(1238, 702);
             this.Controls.Add(this.materialTabSelector1);
             this.Controls.Add(this.materialTabControl1);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
@@ -211,6 +258,7 @@
             this.tabSettings.PerformLayout();
             this.tabStatistics.ResumeLayout(false);
             this.tabStatistics.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -227,6 +275,8 @@
         private System.Windows.Forms.TabPage tabStatistics;
         private MaterialSkin.Controls.MaterialTabSelector materialTabSelector1;
         private MaterialSkin.Controls.MaterialLabel materialLabelAdres;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
     }
 }
 
--- Presentation/Form1.cs
+++ Presentation/Form1.cs
@@ -24,6 +24,8 @@ namespace Presentation
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
             materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+
+
         }
 
         HubConnection connection = null;
@@ -59,16 +61,22 @@ namespace Presentation
                         Invoke(new Action(() =>
                         {
                             logBox.Text = $"{resultingTextStatistics.finishedAutomata.Sum(s => s.solution.solvedB.Count)} total interesting.";
+                            chart1.Series["UnaryFinishedSeries"].Points.Clear();
+                            chart1.Series["UnaryFinishedSeries"].Points.AddXY("Pozostało", resultingTextStatistics.total - resultingTextStatistics.finishedAutomata.Count);
+                            chart1.Series["UnaryFinishedSeries"].Points.AddXY("Wykonano", resultingTextStatistics.finishedAutomata.Count);
+
+
                             if (resultingTextStatistics.finishedAutomata.Count > 0)
                             {
                                 var totalSeconds = (resultingTextStatistics.finishedAutomata.Max(r => r.finishTime) - resultingTextStatistics.finishedAutomata.Min(r => r.issueTime)).TotalSeconds;
-                                int totalMili = (int)(totalSeconds * 1000) - (int)totalSeconds;
+                                int totalMili = (int)(totalSeconds * 1000) - (int)totalSeconds*1000;
                                 var avgSeconds = resultingTextStatistics.finishedAutomata.Count / totalSeconds;
-                                int avgMili = (int)(avgSeconds * 1000) - (int)avgSeconds;
+                                int avgMili = (int)(avgSeconds * 1000) - (int)avgSeconds*1000;
+
                                 logBox.Text += resultingTextStatistics.description + $" Total speed: {avgSeconds:F2}";
 
                                 materialLabel1.Text = "Całkowity czas obliczeń: " + (new TimeSpan(0, 0, 0, (int)totalSeconds, totalMili)).ToString();
-                                materialLabel3.Text = "Średni czas analizy jednego automatu unarnego: " + (new TimeSpan(0, 0, 0, (int)totalSeconds, avgMili)).ToString();
+                                materialLabel3.Text = "Średni czas analizy jednego automatu unarnego: " + (new TimeSpan(0, 0, 0, (int)avgSeconds, avgMili)).ToString();
                             }
                         }));
                     }
