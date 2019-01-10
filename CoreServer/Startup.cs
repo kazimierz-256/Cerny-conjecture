593d75c062705014cb957e59421fec8488cb1248
WPF <VisualStudioWPF@outlook.com>
2019-01-10 03:17:38 +0100

    compiled modules

--- CoreServer/Startup.cs
+++ CoreServer/Startup.cs
@@ -20,7 +20,7 @@ namespace CoreServer
     {
         #region Important parameter
         private int AutomatonProblemSize = 10;
-        private int maximumCount = 30;
+        private int maximumCount = 100;
         #endregion
 
         public Startup(IConfiguration configuration)
