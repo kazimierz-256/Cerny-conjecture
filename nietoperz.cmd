a777acd4b3a217905f66f4c475a6a8cf0e436978
MINI\tumialism <tumialism@student.mini.pw.edu.pl>
2019-01-18 19:07:26 +0100

    Nietoperz

---
+++ nietoperz.cmd
@@ -0,0 +1,17 @@
+cd h:\Windows7\Desktop\Cerny\cernyconjectureautomataproduction
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
+start run-client.cmd http://192.168.144.46:62752 1
