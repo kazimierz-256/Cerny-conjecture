60c5d5d297005af5b2dbff7d495806aaef0f7545
WPF <VisualStudioWPF@outlook.com>
2019-01-24 22:39:05 +0100

    added explanation

--- EXAMPLE-run-four-clients.cmd
+++ EXAMPLE-run-four-clients.cmd
@@ -1,6 +1,9 @@
 :: SERVER
 :: if you would like the server to resume computation using some specified file from `n` state-computation then please launch the following:
-:: start run-server n 20 http://localhost:62752 path/to/file/all-history-n-20-123.xml
+::   start run-server n 20 http://localhost:62752 path/to/file/all-history-n-20-123.xml
+:: parameter explanation:
+:: 8 stands for automaton state count
+:: 20 stands for the number of collected
 start run-server 7 20 http://localhost:62752
 
 :: CLIENTS
